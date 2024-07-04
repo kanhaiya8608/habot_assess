7e98898859e68accce7ef47599f12d37e86bc67c
Kanhaiya Verma <kanhaiya831998@gmail.com>
2024-07-05 00:28:30 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Base alias for src directory
-      '@assets': path.resolve(__dirname, '../src/assets'), // Example alias for ../../assets
-      '@components': path.resolve(__dirname, '../components'), // Example alias for ../components
-      // Add more aliases as needed
-    },
-  },
-});
+})
